956de6ea1944c905be6bf4316ae88b01b248b122
Konstantin Avdeev <fujiun91@gmail.com>
2023-03-21 20:27:55 +0300

    2. Сервис - Кинотеатр [#504869] second commit

---
+++ src/main/java/ru/job4j/cinema/repository/Sql2oUserRepository.java
@@ -0,0 +1,51 @@
+package ru.job4j.cinema.repository;
+
+import net.jcip.annotations.ThreadSafe;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Repository;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+import ru.job4j.cinema.model.User;
+
+import java.util.Optional;
+
+@ThreadSafe
+@Repository
+public class Sql2oUserRepository implements UserRepository {
+
+    private static final Logger LOG = LogManager.getLogger(Sql2oUserRepository.class.getName());
+
+    private final Sql2o sql2o;
+
+    public Sql2oUserRepository(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public Optional<User> save(User user) {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery("INSERT INTO users(fullName, email, password) VALUES(:fullName, :email, :password)", true)
+                    .addParameter("fullName", user.getFullName())
+                    .addParameter("email", user.getEmail())
+                    .addParameter("password", user.getPassword());
+            int generatedId = query.executeUpdate().getKey(Integer.class);
+            user.setId(generatedId);
+            return Optional.of(user);
+        } catch (Sql2oException exception) {
+            LOG.error("Can not save user, some exception: ", exception);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> findByEmailAndPassword(String email, String password) {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery("SELECT * FROM users WHERE email = :email AND password = :password");
+            query.addParameter("email", email).addParameter("password", password);
+            var user = query.executeAndFetchFirst(User.class);
+            return Optional.ofNullable(user);
+        }
+    }
+
+}
